1fba9cf6b5d2f99a799aed89831390e421f515ac
Sava Stanchev <s.stanchev94@gmail.com>
2021-07-06 18:25:27 +0300

    move calendar building into a separate component

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,25 +1,24 @@
+import {useState, useEffect} from 'react';
 import moment from 'moment';
+import {buildCalendar} from './Calendar';
 
 const MainPage = () => {
-  const value = moment();
-  const startDay = value.clone().startOf('month').startOf('week');
-  const endDay = value.clone().endOf('month').endOf('week');
-  const day = startDay.clone().subtract(1, 'day');
-  const calendar = [];
+  const [value, setValue] = useState(moment());
+  const [calendar, setCalendar] = useState([]);
 
-  while(day.isBefore(endDay, 'day')) {
-    calendar.push(
-      Array(7).fill(0).map(() => day.add(1, 'day').clone())
-    );
-  }
+  useEffect(() => {
+    setCalendar(buildCalendar(value));  
+  }, [value])
 
   return(
     <div className="calendar">
       {calendar.map((week) => (
         <div>
           {week.map((day) => (
-            <div className="day">
-              {day.format("D").toString()}
+            <div className="day" onClick={() => setValue(day)}>
+              <div className={value.isSame(day, 'day') ? 'selected' : ''}>
+                {day.format("D").toString()}
+              </div>
             </div>
           ))}
         </div>
