e6428e03231481a3068fde520987632f5605173a
Sava Stanchev <s.stanchev94@gmail.com>
2021-07-06 17:01:15 +0300

    add calendar data

---
+++ client/src/data/data.js
@@ -0,0 +1,197 @@
+export const CalendarData = [
+  {
+    "date": "2021-06-11",
+    "status": "bookable",
+    "price": 98
+  },
+  {
+    "date": "2021-06-12",
+    "status": "blocked",
+    "price": 156
+  },
+  {
+    "date": "2021-06-13",
+    "status": "blocked",
+    "price": 156
+  },
+  {
+    "date": "2021-06-14",
+    "status": "blocked",
+    "price": 142
+  },
+  {
+    "date": "2021-06-15",
+    "status": "bookable",
+    "price": 143
+  },
+  {
+    "date": "2021-06-16",
+    "status": "bookable",
+    "price": 130
+  },
+  {
+    "date": "2021-06-17",
+    "status": "bookable",
+    "price": 115
+  },
+  {
+    "date": "2021-06-18",
+    "status": "bookable",
+    "price": 140
+  },
+  {
+    "date": "2021-06-19",
+    "status": "bookable",
+    "price": 181
+  },
+  {
+    "date": "2021-06-20",
+    "status": "bookable",
+    "price": 202
+  },
+  {
+    "date": "2021-06-21",
+    "status": "bookable",
+    "price": 110
+  },
+  {
+    "date": "2021-06-22",
+    "status": "bookable",
+    "price": 101
+  },
+  {
+    "date": "2021-06-23",
+    "status": "bookable",
+    "price": 101
+  },
+  {
+    "date": "2021-06-24",
+    "status": "bookable",
+    "price": 98
+  },
+  {
+    "date": "2021-06-25",
+    "status": "bookable",
+    "price": 114
+  },
+  {
+    "date": "2021-06-26",
+    "status": "bookable",
+    "price": 173
+  },
+  {
+    "date": "2021-06-27",
+    "status": "bookable",
+    "price": 182
+  },
+  {
+    "date": "2021-06-28",
+    "status": "bookable",
+    "price": 154
+  },
+  {
+    "date": "2021-06-29",
+    "status": "bookable",
+    "price": 138
+  },
+  {
+    "date": "2021-06-30",
+    "status": "bookable",
+    "price": 160
+  },
+  {
+    "date": "2021-07-01",
+    "status": "bookable",
+    "price": 174
+  },
+  {
+    "date": "2021-07-02",
+    "status": "bookable",
+    "price": 200
+  },
+  {
+    "date": "2021-07-03",
+    "status": "bookable",
+    "price": 180
+  },
+  {
+    "date": "2021-07-04",
+    "status": "bookable",
+    "price": 135
+  },
+  {
+    "date": "2021-07-05",
+    "status": "bookable",
+    "price": 113
+  },
+  {
+    "date": "2021-07-06",
+    "status": "bookable",
+    "price": 103
+  },
+  {
+    "date": "2021-07-07",
+    "status": "bookable",
+    "price": 103
+  },
+  {
+    "date": "2021-07-08",
+    "status": "bookable",
+    "price": 105
+  },
+  {
+    "date": "2021-07-09",
+    "status": "bookable",
+    "price": 113
+  },
+  {
+    "date": "2021-07-10",
+    "status": "bookable",
+    "price": 108
+  },
+  {
+    "date": "2021-07-11",
+    "status": "bookable",
+    "price": 119
+  },
+  {
+    "date": "2021-07-12",
+    "status": "bookable",
+    "price": 117
+  },
+  {
+    "date": "2021-07-13",
+    "status": "bookable",
+    "price": 117
+  },
+  {
+    "date": "2021-07-14",
+    "status": "bookable",
+    "price": 106
+  },
+  {
+    "date": "2021-07-15",
+    "status": "bookable",
+    "price": 111
+  },
+  {
+    "date": "2021-07-16",
+    "status": "bookable",
+    "price": 122
+  },
+  {
+    "date": "2021-07-17",
+    "status": "bookable",
+    "price": 119
+  },
+  {
+    "date": "2021-07-18",
+    "status": "bookable",
+    "price": 109
+  },
+  {
+    "date": "2021-07-19",
+    "status": "bookable",
+    "price": 104
+  }
+];
