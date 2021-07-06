24450db1545596131904638b6a4002bfda046caf
Sava Stanchev <s.stanchev94@gmail.com>
2021-07-06 17:43:33 +0300

    install momentjs and create a loop for the days

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,7 +1,30 @@
+import moment from 'moment';
+
 const MainPage = () => {
+  const value = moment();
+  const startDay = value.clone().startOf('month').startOf('week');
+  const endDay = value.clone().endOf('month').endOf('week');
+  const day = startDay.clone().subtract(1, 'day');
+  const calendar = [];
+
+  while(day.isBefore(endDay, 'day')) {
+    calendar.push(
+      Array(7).fill(0).map(() => day.add(1, 'day').clone())
+    );
+  }
 
   return(
-    <div>Hello!</div>
+    <div>
+      {calendar.map((week) => (
+        <div>
+          {week.map((day) => (
+            <div>
+              {day.format("D").toString()}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   )
 };
 
