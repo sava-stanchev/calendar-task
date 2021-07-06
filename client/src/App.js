03e69c0300d4504be6e04150faf47317574f4a7a
Sava Stanchev <s.stanchev94@gmail.com>
2021-07-06 16:19:45 +0300

    add react router dom and main page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Redirect path="/" exact to="/home" />
+          <Route exact path="/home" component={MainPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/components/MainPage.js
@@ -0,0 +1,8 @@
+const MainPage = () => {
+
+  return(
+    <div>Hello!</div>
+  )
+};
+
+export default MainPage;
